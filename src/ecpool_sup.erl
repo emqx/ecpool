acdd822acfb5c08f2c9a38eedcd9fd7f2ff8a6ed
wwhai <cnwwhai@gmail.com>
2021-04-26 13:37:00 +0800

    fix(bug): fix ecpool_pool 'out of range' error

--- src/ecpool_sup.erl
+++ src/ecpool_sup.erl
@@ -51,7 +51,7 @@ stop_pool(Pool) when is_atom(Pool) ->
     ChildId = child_id(Pool),
 	case supervisor:terminate_child(?MODULE, ChildId) of
         ok ->
-            supervisor:delete_child(?MODULE, ChildId);
+            ok = supervisor:delete_child(?MODULE, ChildId);
         {error, Reason} ->
             {error, Reason}
 	end.
